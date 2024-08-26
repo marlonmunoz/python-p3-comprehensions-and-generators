49d3c4d3a202dacebb2e6c62df39b1a8b981e2a4
Marlon Munoz <marlon@marlonshade.com>
2024-08-26 09:56:44 -0400

    com and ge lab completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+num_list = [0,1,2,3,5,7,8,9]
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [word + '!' for word in sentence_list  ]
